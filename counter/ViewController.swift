c1028b41e8acf145412141283b72ee4dad683db5
Swlsky <sawilsky@yandex.ru>
2025-05-24 12:41:45 +0300

    Добавлен private и свойство пересено в начало класса

--- counter/ViewController.swift
+++ counter/ViewController.swift
@@ -8,20 +8,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    @IBOutlet weak var counter: UILabel!
+    private var count = 0
+    
+    @IBOutlet weak private var counter: UILabel!
     @IBOutlet weak var buttonCounter: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
     @IBAction func buttonDidTap(_ sender: Any) {
         print("Нажатие")
         count += 1
         return counter.text = "\(count)"
     }
-    private var count = 0
 }
 
